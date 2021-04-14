56d39c7f1bb6b2568b4cebcd91b93a0b55df1fba
Guy Dar <dar@mail.tau.ac.il>
2021-04-14 14:08:29 +0300

    get data script

---
+++ get_data.sh
@@ -0,0 +1,6 @@
+mkdir datasets
+cd datasets
+wget https://storage.googleapis.com/long-range-arena/lra_release.gz
+tar -xvf lra_release.gz lra_release/listops-1000
+tar -xvf lra_release.gz lra_release/lra_release
+
