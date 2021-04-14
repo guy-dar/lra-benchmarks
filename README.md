e601094522569a2404bcf223395bacbb5bae6e1e
Guy Dar <dar@mail.tau.ac.il>
2021-04-14 18:34:29 +0300

    update model running code and readme

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # lra-benchmark
 
-This repository is based on Long Range Arena (LRA) datasets from Google Research. The original repository by Google is available [here](https://github.com/google-research/long-range-arena).
-In this repo we provide researchers code to work with some of the simple tasks. It also has the advantage of using PyTorch and HuggingFace Transformers instead of Jax/Flax, with which less are familiar.
+This repository is based on Long Range Arena (LRA) datasets from Google Research. The original repository by Google is available [here](https://github.com/google-research/long-range-arena). The paper is available on [ArXiv](https://arxiv.org/pdf/2011.04006.pdf).
+In this repo we provide researchers code to work with some of the simpler tasks. It also has the advantage of using PyTorch and HuggingFace Transformers instead of Jax/Flax, with which less are familiar.
 
 ## Use Benchmark
-1. Run `sh ./get_data.sh`
-2. [TBD]
+1. Run `sh ./get_data.sh`. It will create a new `datasets` directory in the project and will populate it with the required datasets (make sure to run this in the project's root directory as the script uses relative paths)
+2. To test the code with simple models, just run: `python run_model.py`
--- run_model.py
+++ run_model.py
@@ -1,21 +1,29 @@
+import pandas as pd
 import torch
 from torch import nn
+import torch.nn.functional as F
 import transformers
-from transformers import AutoModelForSequenceClassification, AutoModelForSequenceClassification
+from transformers import (AutoTokenizer, AutoModelForSequenceClassification, AutoConfig)
 from tqdm import tqdm
 from torch.optim import Adam
 from torch.optim.lr_scheduler import MultiplicativeLR
 from torch.utils.data import DataLoader
 
+# helper fns
+def dict_to_device(inputs, device):
+    return {key: inputs[key].to(device) for key in inputs}
+
+# datasets
 class ListOpsDataset:
     def __init__(self, tokenizer):
-        data_path = "datasets/listops-1000/basic_train.tsv"
+        data_path = "datasets/lra_release/listops-1000/basic_train.tsv"
         self.data = pd.read_csv(data_path, delimiter='\t')
         self.tokenizer = tokenizer
         
     def __getitem__(self, i):
         data = self.data.iloc[i]
-        inputs = self.tokenizer(data.Source, return_tensors='pt')
+        source = data.Source
+        inputs = self.tokenizer(source, return_tensors='pt', truncation=True, max_length=512, padding='max_length')
         target = data.Target
         return inputs, torch.LongTensor([target])
     
@@ -23,34 +31,41 @@ class ListOpsDataset:
         return len(self.data)
     
 # config
-config = {
-    "n_hidden_layers": 6,
-    "max_length": 2048,    
-}
+model_name = "bert-base-uncased"
+config = AutoConfig.from_pretrained(model_name)
+config.num_labels = 10
+
 # hyperparams
+device = 'cuda'
 epochs = 1
 lr = 1e-4
-batch_size = 4
+batch_size = 1 #TODO: allow batching 
 
 # model
-model = AutoModelForSequenceClassification.from_config(**config)
-tokenizer = AutoTokenizer.from_config(**config)
+model = AutoModelForSequenceClassification.from_config(config)
+model.to(device)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 optimizer = Adam(model.parameters(), lr=lr)
-scheduler = MultiplicativeLR(lambda x: 1)
+scheduler = MultiplicativeLR(optimizer, lambda x: 1)
 # load data
-datset = ListOpsDataset(tokenizer)
-dataloader = DataLoader(dataset, batch_size = batch_size)
+dataset = ListOpsDataset(tokenizer)
+dataloader = dataset #DataLoader(dataset, batch_size = batch_size)
 
 # train model
+model.train()
 for _ in range(epochs):
     running_loss = 0
+    running_acc = 0
     pbar = tqdm(dataloader)
     for i, (inputs, target) in enumerate(pbar):
         optimizer.zero_grad()
+        inputs = dict_to_device(inputs, device)
+        target = target.to(device)
         outputs = model(**inputs)
-        loss = F.cross_entropy(outputs, target)
+        loss = F.cross_entropy(outputs.logits, target)
         loss.backward()
         running_loss += loss.item()
-        pbar.set_postfix_str(f"loss: {running_loss/(i+1)}")
+        running_acc += sum(torch.argmax(loss, dim=-1) == target).item()/batch_size
+        pbar.set_postfix_str(f"loss: {running_loss/(i+1):.2f} accuracy: {running_acc/(i+1):.2f}")
         optimizer.step()
         scheduler.step()
