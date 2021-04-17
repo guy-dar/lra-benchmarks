a1d95fc4899fe91fe38fd37cf723ebaf6300bc1f
Guy Dar <dar@mail.tau.ac.il>
2021-04-17 23:11:55 +0300

    fix get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -1,7 +1,7 @@
 mkdir datasets
 cd datasets
 wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
-tar -xvf cifar-10-python
+tar -xvf cifar-10-python.tar.gz
 wget https://storage.googleapis.com/long-range-arena/lra_release.gz
 tar -xvf lra_release.gz lra_release/listops-1000
 tar -xvf lra_release.gz lra_release/lra_release/tsv_data
